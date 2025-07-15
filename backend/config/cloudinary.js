b3d887c2d28042a9aac866f0a21ef591be1062d1
Madhav Acharya <np05cp4s240068@iic.edu.np>
2025-07-15 16:16:26 +0545

    removed unnecessary console from the cloudinary api leaking

--- backend/config/cloudinary.js
+++ backend/config/cloudinary.js
@@ -7,5 +7,4 @@ cloudinary.config({
   api_key: process.env.CLOUDINARY_API_KEY,
   api_secret: process.env.CLOUDINARY_API_SECRET,
 });
-console.log("cloudAPI", process.env.CLOUDINARY_API_KEY)
 export default cloudinary;
